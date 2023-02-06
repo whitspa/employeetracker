d63066d377fddfbaa0edf80472fa20675baed3bb
whit <whitspa64@gmail.com>
2023-02-06 10:47:23 -0500

    required consol.table node package. worked on api CRUD api routes

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require('express');
-const inquirer = require('inquirer');
-
-// Import and require mysql2
 const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+
+function init() {
+  inquirer
+  .prompt([type: "list",
+  name: "Departments",
+  message: "Please select what you would like to review",
+  choices: [
+  "View all Departments",
+  "View all Roles",
+  "View all Employees",
+  ]
+])
+},
+
+.then((response) => {}
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -24,6 +39,121 @@ const db = mysql.createConnection(
   console.log(`Connected to the employees_db database.`)
 );
 
+// Create a department
+app.post('/api/new-department', ({ body }, res) => {
+  const sql = `INSERT INTO department (department_name)
+    VALUES (?)`;
+  const params = [body.department_name];
+  
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: body
+    });
+  });
+});
+
+// Create a role
+app.post('/api/new-role', ({ body }, res) => {
+  const sql = `INSERT INTO role (role_title)
+    VALUES (?)`;
+  const params = [body.department_name];
+  
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: body
+    });
+  });
+});
+
+
+// Create an employee
+app.post('/api/new-employee', ({ body }, res) => {
+  const sql = `INSERT INTO role (employee_first_name)
+    VALUES (?)`;
+  const params = [body.department_name];
+  
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: body
+    });
+  });
+});
+
+// View all Departments
+
+app.get('/api/department', (req, res) => {
+  const sql = `SELECT id, department_name AS department FROM department`;
+  
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+       return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
+
+// Update department
+app.put('/api/department/:id', (req, res) => {
+  const sql = `UPDATE department SET department = ? WHERE id = ?`;
+  const params = [req.body.review, req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'department not found'
+      });
+    } else {
+      res.json({
+        message: 'success',
+        data: req.body,
+        changes: result.affectedRows
+      });
+    }
+  });
+});
+
+// Delete a department
+app.delete('/api/department/:id', (req, res) => {
+  const sql = `DELETE FROM department WHERE id = ?`;
+  const params = [req.params.id];
+  
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.statusMessage(400).json({ error: res.message });
+    } else if (!result.affectedRows) {
+      res.json({
+      message: 'department not found'
+      });
+    } else {
+      res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+      });
+    }
+  });
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
